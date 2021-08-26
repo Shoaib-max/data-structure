3218a4f852a7f659831972e5952ba9737415abba
AnkitRaj5 <ankitraj6241@gmail.com>
2021-08-26 22:57:29 +0530

    created a .c file for you guys to contribute

---
+++ sorting.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+// i adding here hello world
+printf("hello world");
+// now form here you guys add sorting algorithms
+//praduman
+
+// shoaib
+
+// shalni
+
+return 0;
+}
